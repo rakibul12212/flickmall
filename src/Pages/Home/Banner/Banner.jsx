43b699ce53c42f549c3a766845432b1f4653cff7
rakibul12212 <rakibul15-12212@diu.edu.bd>
2024-06-02 20:53:36 +0600

    banner carosul

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,41 +1,174 @@
+import "./Banner.css";
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+// import required modules
+import { Pagination } from "swiper/modules";
 const Banner = () => {
   return (
-    <div className="container px-5 md:px-20 max-w-[500px]">
-      <div className="pt-5 border-r-2">
-        <div className="flex flex-col items-start px-4 font-semibold">
+    <div className="px-[80px] flex justify-between  ">
+      <div className="max-w-[300px] pt-4 border-r-2">
+        <div className="flex flex-col items-start px-4  font-semibold">
           <select
             id="category"
             name="category"
-            className="px-2 py-4 outline-none rounded text-base w-64 bg-white "
+            className="px-2 py-4 md:pb-4 lg:pb-8 outline-none rounded text-base w-64 bg-white"
           >
-            <option value="technology" className="font-semibold">Womans Fashion</option>
-            <option value="science" className="font-semibold">Item-1</option>
-            <option value="health" className="font-semibold">Item-2</option>
+            <option value="womans-fashion" className="font-semibold">
+              Womans Fashion
+            </option>
+            <option value="item-1" className="font-semibold">
+              Item-1
+            </option>
+            <option value="item-2" className="font-semibold">
+              Item-2
+            </option>
           </select>
         </div>
         <div className="flex flex-col items-start px-4 font-semibold">
           <select
             id="category"
             name="category"
-            className="px-2 pb-4 outline-none rounded text-base w-64 bg-white "
+            className="px-2 md:pb-4 lg:pb-8 outline-none rounded text-base w-64 bg-white"
           >
-            <option value="technology" className="font-semibold">Mans Fashion</option>
-            <option value="science" className="font-semibold">Item-1</option>
-            <option value="health" className="font-semibold">Item-2</option>
+            <option value="mans-fashion" className="font-semibold">
+              Mans Fashion
+            </option>
+            <option value="item-1" className="font-semibold">
+              Item-1
+            </option>
+            <option value="item-2" className="font-semibold">
+              Item-2
+            </option>
           </select>
         </div>
-        <div className="flex flex-col items-start px-7 ">
-          <a href="/" className="pb-4 font-semibold">Electronics</a>
-          <a href="/" className="pb-4 font-semibold">Home & Lifstyle</a>
-          <a href="/" className="pb-4 font-semibold">Medicine</a>
-          <a href="/" className="pb-4 font-semibold">Sports & Outdoor</a>
-          <a href="/" className="pb-4 font-semibold">Baby&apos;s & Toys</a>
-          <a href="/" className="pb-4 font-semibold">Groceries & Pets</a>
-          <a href="/" className="pb-4 font-semibold">Health & Beauty</a>
+        <div className="flex flex-col items-start px-7">
+          <a href="/" className="md:pb-4 lg:pb-8  font-semibold">
+            Electronics
+          </a>
+          <a href="/" className="md:pb-4 lg:pb-8 font-semibold">
+            Home & Lifestyle
+          </a>
+          <a href="/" className="md:pb-4 lg:pb-8 font-semibold">
+            Medicine
+          </a>
+          <a href="/" className="md:pb-4 lg:pb-8 font-semibold">
+            Sports & Outdoor
+          </a>
+          <a href="/" className="md:pb-4 lg:pb- font-semibold">
+            Baby&apos;s & Toys
+          </a>
+          <a href="/" className="md:pb-4 lg:pb-8 font-semibold">
+            Groceries & Pets
+          </a>
+          <a href="/" className=" font-semibold">
+            Health & Beauty
+          </a>
         </div>
+      </div>
+      <div className="max-w-full max-h-[500px] pt-5 overflow-hidden sm:ps-5 md:ps-5 lg:ps-10">
+        <Swiper
+          pagination={{
+            dynamicBullets: true,
+          }}
+          modules={[Pagination]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <img
+              src="https://appletoolbox.com/wp-content/uploads/2020/10/iPhone-12-Pro-Max-Camera-System.png"
+              alt=""
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://media.glamour.com/photos/65cce0375ea0c25e5b06764f/master/w_2560%2Cc_limit/spring%25202024%2520fashion%2520trends.jpg"
+              alt=""
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://static.vecteezy.com/system/resources/previews/027/295/652/non_2x/successful-businessman-in-leather-jacket-observing-documents-outside-office-empty-space-for-copy-free-photo.jpg"
+              alt=""
+            />
+          </SwiperSlide>
+        </Swiper>
       </div>
     </div>
   );
 };
 
 export default Banner;
+
+{
+  /* <div className="container px-5 md:px-20 flex border">
+      
+      <div className="pt-5 border-r-2 ">
+        <div className="flex flex-col items-start px-4 font-semibold">
+          <select
+            id="category"
+            name="category"
+            className="px-2 py-4 outline-none rounded text-base w-64 bg-white"
+          >
+            <option value="womans-fashion" className="font-semibold">
+              Womans Fashion
+            </option>
+            <option value="item-1" className="font-semibold">
+              Item-1
+            </option>
+            <option value="item-2" className="font-semibold">
+              Item-2
+            </option>
+          </select>
+        </div>
+        <div className="flex flex-col items-start px-4 font-semibold">
+          <select
+            id="category"
+            name="category"
+            className="px-2 pb-4 outline-none rounded text-base w-64 bg-white"
+          >
+            <option value="mans-fashion" className="font-semibold">
+              Mans Fashion
+            </option>
+            <option value="item-1" className="font-semibold">
+              Item-1
+            </option>
+            <option value="item-2" className="font-semibold">
+              Item-2
+            </option>
+          </select>
+        </div>
+        <div className="flex flex-col items-start px-7">
+          <a href="/" className="pb-4 font-semibold">
+            Electronics
+          </a>
+          <a href="/" className="pb-4 font-semibold">
+            Home & Lifestyle
+          </a>
+          <a href="/" className="pb-4 font-semibold">
+            Medicine
+          </a>
+          <a href="/" className="pb-4 font-semibold">
+            Sports & Outdoor
+          </a>
+          <a href="/" className="pb-4 font-semibold">
+            Baby&apos;s & Toys
+          </a>
+          <a href="/" className="pb-4 font-semibold">
+            Groceries & Pets
+          </a>
+          <a href="/" className="pb-4 font-semibold">
+            Health & Beauty
+          </a>
+        </div>
+      </div>
+      <div className="ps-10 border w-full"> 
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse, odit vero. Odio ab autem, voluptatem cum aliquid velit doloremque assumenda ea, tempora accusamus reprehenderit? Pariatur.</p>
+      </div>
+    
+  </div> */
+}

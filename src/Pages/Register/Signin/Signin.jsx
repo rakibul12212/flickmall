26975562b10118a20789de0db9ae46c7eb71374b
rakibul12212 <rakibul15-12212@diu.edu.bd>
2024-06-09 02:57:40 +0600

    register page

--- src/Pages/Register/Signin/Signin.jsx
+++ src/Pages/Register/Signin/Signin.jsx
@@ -1,11 +1,52 @@
-import React from 'react';
+import React from "react";
+import Container from "../../../Component/Ui/Container";
 
 const Signin = () => {
-    return (
-        <div>
-            <p>sign in</p>
+  return (
+    <Container>
+      <div className="px-28 py-10">
+        <div className="flex justify-center items-center gap-16 ">
+          <div>
+            <img
+              src="https://i.ibb.co/F6YwCRN/Side-Image.png"
+              alt="img"
+              className="max-w-full h-[600px] hidden md:block"
+            />
+          </div>
+          <div className="flex flex-col justify-center pr-20">
+            <p className="font-semibold text-4xl">Log In to FlickMall</p>
+            <p className="py-5">Enter your details below</p>
+            <div>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="enter your email"
+                className="border-b-2 w-[300px] my-5 pb-2 outline-none"
+              />
+              <br />
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="password"
+                className="border-b-2 w-[300px]  pb-2 outline-none"
+              />
+              <div className="pt-5 flex justify-between items-center gap-x-4">
+                <button className="bg-red-500 text-white hover:bg-red-700 px-6 py-2">
+                  Sign in
+                </button>
+                <a href="signin" className="text-red-500">forget password ?</a>
+              </div>
+              <div className="pt-5">
+                <p>Don't have an account ? <a href="signup" className="text-red-500">Sign Up</a></p>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </Container>
+  );
 };
 
 export default Signin;
--- src/Pages/Register/Signup/Signup.jsx
+++ src/Pages/Register/Signup/Signup.jsx
@@ -1,10 +1,55 @@
 import React from 'react';
+import Container from '../../../Component/Ui/Container';
 
 const Signup = () => {
     return (
-        <div>
-            <p>signup</p>
+        <Container>
+        <div className="px-28 py-10">
+          <div className="flex justify-center items-center gap-16 ">
+            <div>
+              <img
+                src="https://i.ibb.co/F6YwCRN/Side-Image.png"
+                alt="img"
+                className="max-w-full h-[600px] hidden md:block"
+              />
+            </div>
+            <div className="flex flex-col justify-center pr-20">
+              <p className="font-semibold text-4xl">Log In to FlickMall</p>
+              <p className="py-5">Enter your details below</p>
+              <div>
+                <input type="text" name="name" id="name"  placeholder="name"
+                  className="border-b-2 w-[300px] outline-none pb-2"/>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="email"
+                  className="border-b-2 w-[300px] outline-none pb-2 my-5"
+                />
+                <br />
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="password"
+                  className="border-b-2 w-[300px] outline-none pb-2"
+                />
+                <div className="pt-5">
+                  <button className="bg-red-500 text-white hover:bg-red-700 w-[300px] py-3 mb-2">
+                    Sign Up
+                  </button>
+                  <button className="bg-transparent hover:bg-red-700  text-black hover:text-white border-2  w-[300px] py-3">
+                    <div className='flex justify-center items-center gap-2'><img src="https://i.ibb.co/G7StqsP/Icon-Google.png" alt="" /> <p>Sign Up with google</p></div>
+                  </button>
+                </div>
+                <div className='pt-5'>
+                    <p>Already have an account ? <a href="signin" className='text-red-500'>Sign in</a></p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+      </Container>
     );
 };
 
